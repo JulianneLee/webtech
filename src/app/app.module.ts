24f36ef6fab3e21d7832ec9926cddfbe5957f30c
junbin98 <junbin.tan@gmail.com>
2020-09-25 21:45:54 +0800

    changes layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'
 import { NgModule } from '@angular/core';
 import { AppRoutingModule, routingComponents } from './app-routing.module'
-import { MatPaginatorModule } from '@angular/material/paginator'
+import { NoopAnimationsModule } from '@angular/platform-browser/animations'
+import { DemoMaterialModule } from './material-module';
+
+// import { MatPaginatorModule } from '@angular/material/paginator'
 //import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog'
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSidenavModule } from '@angular/material/sidenav'
-import { MatGridListModule } from '@angular/material/grid-list'
-import { MatCardModule } from '@angular/material/card'
-import { MatTableModule } from '@angular/material/table';
-import { MatDialogModule } from '@angular/material/dialog'
+// import { MatSidenavModule } from '@angular/material/sidenav'
+// import { MatGridListModule } from '@angular/material/grid-list'
+// import { MatCardModule } from '@angular/material/card'
+// import { MatTableModule } from '@angular/material/table';
+// import { MatDialogModule } from '@angular/material/dialog'
 
 import { AppComponent } from './app.component';
 import { TestCaseComponent } from './test-case/test-case.component'
@@ -24,13 +26,14 @@ import { TestCaseComponent } from './test-case/test-case.component'
     BrowserModule,
     NoopAnimationsModule,
     AppRoutingModule,
-    MatSidenavModule,
-    MatPaginatorModule,
+    DemoMaterialModule,
     FormsModule,
-    MatGridListModule,
-    MatCardModule,
-    MatTableModule,
-    MatDialogModule
+    // MatSidenavModule,
+    // MatPaginatorModule,
+    // MatGridListModule,
+    // MatCardModule,
+    // MatTableModule,
+    // MatDialogModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
