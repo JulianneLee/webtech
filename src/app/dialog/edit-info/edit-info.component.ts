01e225fc26fcbaddc948701984e3f70e69306ba7
Julianne <juliannelrp@yahoo.com>
2020-09-27 02:48:48 +0800

    edit and update

--- src/app/dialog/edit-info/edit-info.component.ts
+++ src/app/dialog/edit-info/edit-info.component.ts
@@ -3,7 +3,7 @@ import { NgForm } from '@angular/forms'
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
-  selector: 'dialog-add-patient',
+  selector: 'dialog-edit-info',
   templateUrl: 'edit-info.component.html',
 })
 
